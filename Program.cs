cf1983b3389bfca35abdb64bcb75fc7a19b51227
Zou Wei <zwcloud@hotmail.com>
2018-11-24 20:43:04 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace MonoWasmDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Image<Rgba32> image = new Image<Rgba32>(100,100);
+            Console.WriteLine("Hello from C#! {0}x{1}", image.Width, image.Height);
+        }
+    }
+}
---
+++ package.ps1
@@ -0,0 +1,1 @@
+./MonoWasmSdk/packager.exe --copy=ifnewer --out=.\publish --asset=index.html .\bin\debug\netstandard2.0\MonoWasmDemo.dll
