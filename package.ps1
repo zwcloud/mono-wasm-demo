4ff96b291fd6c008c2cd821b26e098c682e635cd
Zou Wei <zwcloud@hotmail.com>
2018-11-24 20:45:51 +0800

    fix path

--- package.ps1
+++ package.ps1
@@ -1,1 +1,1 @@
-./MonoWasmSdk/packager.exe --copy=ifnewer --out=.\publish --asset=index.html .\bin\debug\netstandard2.0\MonoWasmDemo.dll
+./MonoWasm/packager.exe --copy=ifnewer --out=.\publish --asset=index.html .\bin\debug\netstandard2.0\MonoWasmDemo.dll
